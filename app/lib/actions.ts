6829c805cfb61f6872266f3c40e0c2d3ed65a746
jamisays <jamisays@gmail.com>
2025-06-23 06:42:27 +0600

    updated sucure - false & .prod env public url

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -51,21 +51,21 @@ export async function handleRefresh() {
 export async function handleLogin(userId: string, accessToken: string, refreshToken: string) {
   (await cookies()).set('session_userId', userId, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: false,
       maxAge: 60 * 60 * 24 * 7, // one week
       path: '/'
   });
 
   (await cookies()).set('session_access_token', accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: false,
       maxAge: 60 * 60, // one day
       path: '/'
   });
 
   (await cookies()).set('session_refresh_token', refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: false,
       maxAge: 60 * 60 * 24 * 7, // one week
       path: '/'
   });
